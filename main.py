9754e984365481d8412c2c43307531d6d28760f5
sflorek01 <sflorek01@gmail.com>
2023-07-19 14:57:30 -0400

    updated function name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def encode_num(num_str):
+def encode(num_str):
     encoded_str = ""
     for num in num_str:
         encoded_str += str((int(num) + 3) % 10)
@@ -17,7 +17,7 @@ def main():
         menu_choice = int(input("Please enter an option: "))
         if menu_choice == 1:
             password = input("Please enter your password to encode: ")
-            encoded_password = encode_num(password)
+            encoded_password = encode(password)
             print("Your password has been encoded and stored!\n")
         # implement decode function here
         elif menu_choice == 3:
